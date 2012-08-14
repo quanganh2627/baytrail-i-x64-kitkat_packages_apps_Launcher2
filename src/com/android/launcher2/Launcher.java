8df669f0cb738d3e08b8683c7dc96ec775401692
Yong Yao <yong.yao@intel.com>
2012-08-14 08:59:30 +0800

    [PORT FROM MAIN]Launcher: Fix the Null point issue for mWorkspace
    BZ: 51436
    
    mWorkspace may have been destroyed when onAnimationEnd is called
    
    Feature: AOSP-Home
    Category: aosp improvement
    Severity: medium
    Origin: internal
    Upstream-Candidate: Yes
    Orig-Change-Id: Ic6985dd5bfb784650cac3ad7577c713126ad1740
    Change-Id: Icaac0bc3ce05423f50ab0ed31b63b1f0015f8afe
    Signed-off-by: Yong Yao <yong.yao@intel.com>

--- src/com/android/launcher2/Launcher.java
+++ src/com/android/launcher2/Launcher.java
@@ -2626,7 +2626,9 @@ public final class Launcher extends Activity
 
                     if (mWorkspace != null && !springLoaded && !LauncherApplication.isScreenLarge()) {
                         // Hide the workspace scrollbar
-                        mWorkspace.hideScrollingIndicator(true);
+                        if (mWorkspace != null) {
+                            mWorkspace.hideScrollingIndicator(true);
+                        }
                         hideDockDivider();
                     }
                     if (!animationCancelled) {
