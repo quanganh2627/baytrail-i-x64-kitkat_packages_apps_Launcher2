a9b7fcf99d992d2b4489786f709cdfd9151aac12
Andre Amegah <andrex.amegah@intel.com>
2012-10-31 19:57:27 +0100

    [PORT FROM MAIN] Launcher2: Support for STK User Activity Event.
    BZ: 135986
    
    Stk User Activity Event is not supported in Android
    framework.
    
    This patch provides support for User Activity event.
    
    User activity event is an STK Event Dowload commands.
    When UICC registers to this event via a Setup Event List
    command and next the terminal detects any user key-press
    or screen-touch, the terminal informs UICC through an Stk
    Envelope command.
    This is oneshot event as after sending the envelope,
    User Activity event is removed from Event List.
    
    Orig-Change-Id: I5bd14bbaab89bd9d03b3659d1d8e728b0479335d
    Category: device enablement
    Domain: Telephony-Modem-Common
    Origin: internal
    Upstream-Candidate: yes
    Change-Id: I7546ef5471317041cc362eb59305dcdacca95741
    Signed-off-by: Andre Amegah <andrex.amegah@intel.com>

--- src/com/android/launcher2/Launcher.java
+++ src/com/android/launcher2/Launcher.java
@@ -189,6 +189,14 @@ public final class Launcher extends Activity
     private static final String TOOLBAR_VOICE_SEARCH_ICON_METADATA_NAME =
             "com.android.launcher.toolbar_voice_search_icon";
 
+    // Broadcast to be received to start user activity listening.
+    private static final String CHECK_USER_ACTIVITY_ACTION =
+            "android.intent.action.launcher.user_activity";
+
+    // Broadcast to be sent to user activity listener
+    private static final String USER_ACTIVITY_AVAILABLE_ACTION =
+            "android.intent.action.stk.user_activity_available";
+
     /** The different states that Launcher can be in. */
     private enum State { NONE, WORKSPACE, APPS_CUSTOMIZE, APPS_CUSTOMIZE_SPRING_LOADED };
     private State mState = State.WORKSPACE;
@@ -315,6 +323,26 @@ public final class Launcher extends Activity
     private HideFromAccessibilityHelper mHideFromAccessibilityHelper
         = new HideFromAccessibilityHelper();
 
+    /**
+     * Check to see if user activity is requested.
+     *
+     */
+    private boolean mIsUserActivityRequest = false;
+
+    /**
+     * User activity BroadcastReceiver instance.
+     *
+     */
+    private final BroadcastReceiver mUserActivityIntentReceiver = new BroadcastReceiver () {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(CHECK_USER_ACTIVITY_ACTION)) {
+                mIsUserActivityRequest = intent.getBooleanExtra("STK_USER_ACTIVITY_REQUEST", false);
+                Log.d(TAG, "Receiving STK_USER_ACTIVITY_REQUEST "+ mIsUserActivityRequest);
+            }
+        }
+    };
+
     private Runnable mBuildLayersRunnable = new Runnable() {
         public void run() {
             if (mWorkspace != null) {
@@ -426,6 +454,8 @@ public final class Launcher extends Activity
 
         IntentFilter filter = new IntentFilter(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
         registerReceiver(mCloseSystemDialogsReceiver, filter);
+        IntentFilter userActivityfilter = new IntentFilter(CHECK_USER_ACTIVITY_ACTION);
+        registerReceiver(mUserActivityIntentReceiver, userActivityfilter);
 
         updateGlobalIcons();
 
@@ -1602,6 +1632,7 @@ public final class Launcher extends Activity
 
         getContentResolver().unregisterContentObserver(mWidgetObserver);
         unregisterReceiver(mCloseSystemDialogsReceiver);
+        unregisterReceiver(mUserActivityIntentReceiver);
 
         mDragLayer.clearAllResizeFrames();
         ((ViewGroup) mWorkspace.getParent()).removeAllViews();
@@ -1951,6 +1982,7 @@ public final class Launcher extends Activity
 
     @Override
     public boolean dispatchKeyEvent(KeyEvent event) {
+        onUserInteraction(event);
         if (event.getAction() == KeyEvent.ACTION_DOWN) {
             switch (event.getKeyCode()) {
                 case KeyEvent.KEYCODE_HOME:
@@ -1968,8 +2000,14 @@ public final class Launcher extends Activity
                     return true;
             }
         }
-
         return super.dispatchKeyEvent(event);
+    }
+
+    /** @hide */
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        onUserInteraction(event);
+        return super.dispatchTouchEvent(event);
     }
 
     @Override
@@ -1989,6 +2027,23 @@ public final class Launcher extends Activity
             // Back button is a no-op here, but give at least some feedback for the button press
             mWorkspace.showOutlinesTemporarily();
         }
+    }
+
+    /**
+     * Use to broadcast user activity event intent.
+     */
+    private void onUserInteraction(Object event) {
+        if (mIsUserActivityRequest) {
+            mIsUserActivityRequest = false;
+            Intent intent = new Intent(USER_ACTIVITY_AVAILABLE_ACTION);
+            if (event instanceof KeyEvent) {
+                intent.putExtra("EVENT", "key");
+                intent.putExtra("KEY_CODE", ((KeyEvent)event).getKeyCode());
+            } else {
+                intent.putExtra("EVENT", "touch");
+            }
+            sendBroadcast(intent);
+         }
     }
 
     /**
