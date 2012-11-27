f682faf64563ce59b2e9be61cfd1aca84f03045e
Jun Wu <junx.wu@intel.com>
2012-11-27 16:19:26 +0800

    [PORT FROM MAIN] Launcher: fix crash when call new LoaderTask thread's runBindSynchronousPage before old LoaderTask thread stopped.
    BZ: 66274 67779
    
    when mIsLoaderTaskRunning is true it will raise up java.lang.RuntimeException.
    
    Make sure mIsLoaderTaskRunning == false when new LoaderTask ready to run.
    stopLoader() will be called everytime before startLoader() then it can
    avoid java.lang.RuntimeExcepion.
    
    Category: aosp improvement
    Domain: AOSP-Applications
    Severity: high
    Origin: internal
    Upstream-Candidate: no,needs rework
    
    Orig-Change-Id: Ic8405ceba85210d2dc58047830d1b7d901a08813
    Change-Id: I2c473f2f499d2b6aabe69025e4e3d76014746809
    Signed-off-by: Jun Wu <junx.wu@intel.com>

--- src/com/android/launcher2/LauncherModel.java
+++ src/com/android/launcher2/LauncherModel.java
@@ -947,6 +947,7 @@ public class LauncherModel extends BroadcastReceiver {
         synchronized (mLock) {
             if (mLoaderTask != null) {
                 mLoaderTask.stopLocked();
+                mIsLoaderTaskRunning = false;
             }
         }
     }
