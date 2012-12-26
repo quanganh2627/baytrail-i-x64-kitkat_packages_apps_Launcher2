13cc97d8b3c15c59da03fec91830d8e83fad2a72
Wu, JunX <junx.wu@intel.com>
2012-12-26 10:26:26 +0800

    Home: fix JAVACRASH happened when launching default camera repeatedly.
    BZ: 76641
    
    When mWorkspace is null it will throw NullPointerException exception.
    
    Validate whether mWorkspace is null before using it.
    
    Change-Id: I2766c3e3ddbfb0eb0ad00202a83a68be7c32ca77
    Signed-off-by: Wu, JunX <junx.wu@intel.com>
    Reviewed-on: http://android.intel.com:8080/83783
    Reviewed-by: Zhang, Hongyu <hongyu.zhang@intel.com>
    Reviewed-by: Chen, HuaqiangX <huaqiangx.chen@intel.com>
    Tested-by: Li, XiaojingX <xiaojingx.li@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- src/com/android/launcher2/Launcher.java
+++ src/com/android/launcher2/Launcher.java
@@ -1426,6 +1426,9 @@ public final class Launcher extends Activity
 
             Runnable processIntent = new Runnable() {
                 public void run() {
+                    if (mWorkspace == null) {
+                        return;
+                    }
                     Folder openFolder = mWorkspace.getOpenFolder();
                     // In all these cases, only animate if we're already on home
                     mWorkspace.exitWidgetResizeMode();
